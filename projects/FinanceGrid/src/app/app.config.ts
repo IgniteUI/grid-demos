ac63c1781948e02907afcfa80cd435ccf5ac9811
Martin Dragnev <mddragnev@gmail.com>
2024-12-05 16:49:26 +0200

    Remove routes

--- projects/FinanceGrid/src/app/app.config.ts
+++ projects/FinanceGrid/src/app/app.config.ts
@@ -1,17 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
 import {
   provideClientHydration,
   withEventReplay,
-} from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+} from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimations(),
   ],
