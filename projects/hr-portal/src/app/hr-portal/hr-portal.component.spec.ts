a255847fa7b9f8f1ff4e5627fcafe9248ea3223c
ttonev <ttonev@infragistics.com>
2024-12-11 10:52:10 +0200

    changed folder name to lowercase

---
+++ projects/hr-portal/src/app/hr-portal/hr-portal.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  IgxCheckboxModule,
+  IgxDatePickerModule,
+  IgxTreeGridModule,
+} from 'igniteui-angular';
+import { HrPortalComponent } from './hr-portal.component';
+
+describe('TreeGrid1Component', () => {
+  let component: HrPortalComponent;
+  let fixture: ComponentFixture<HrPortalComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        IgxTreeGridModule,
+        IgxDatePickerModule,
+        IgxCheckboxModule,
+        HrPortalComponent,
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HrPortalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
