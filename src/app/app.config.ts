c9c6c0f7c84b987c080852038427d2aa436b4d13
skrustev <striker18@mail.bg>
2024-12-13 09:18:26 +0200

    Remove ssr from main app.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideHttpClient } from '@angular/common/http';
 
@@ -11,6 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimations()]
 };
