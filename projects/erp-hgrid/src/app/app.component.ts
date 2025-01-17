ac871e64e49d16aa026c1dad3951a72a673e4cd6
Svetoslav Krastev <striker18@mail.bg>
2025-01-17 11:10:10 +0200

    Include ERP HGrid sample in to base app. Pre config for Fleet Management app. (#19)

--- projects/erp-hgrid/src/app/app.component.ts
+++ projects/erp-hgrid/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErpHgridSampleComponent } from './erp-hgrid-sample/erp-hgrid-sample.component';
+import { ErpHGridSampleComponent } from './erp-hgrid-sample/erp-hgrid-sample.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, ErpHgridSampleComponent],
+    imports: [CommonModule, ErpHGridSampleComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
