de8b636c6cdd481584de459399d5b05478d34357
Svetoslav Krastev <striker18@mail.bg>
2025-02-19 20:45:59 +0200

    Make disabled download button and link interactive. (#77)

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -26,54 +26,49 @@ export class HomeComponent {
       title: "ERP/ Inventory",
       theme: "Material Light",
       content: "Tracking and managing quantity, location and details of products in stock.",
-      moreLink: "https://github.com/IgniteUI/grid-demos/tree/vnext/projects/erp-hgrid",
+      moreLink: "https://www.infragistics.com/products/ignite-ui-angular/angular/components/hierarchicalgrid/hierarchical-grid",
       downloadLink: ""
     }],
     ['hr-portal', {
       title: "Org Chart/HR Portal",
       theme: "Fluent Light",
       content: "Displaying company's hierarchical structure and showing employees data.",
-      moreLink: "https://github.com/IgniteUI/grid-demos/tree/vnext/projects/hr-portal",
+      moreLink: "https://www.infragistics.com/products/ignite-ui-angular/angular/components/treegrid/tree-grid",
       downloadLink: ""
     }],
     ['finance', {
       title: "Financial Portfolio",
       theme: "Bootstrap Light",
       content: "Asset tracking, profit and loss analysis, featuring interactive dynamic charts.",
-      moreLink: "https://github.com/IgniteUI/grid-demos/tree/vnext/projects/finance-grid",
+      moreLink: "https://www.infragistics.com/products/ignite-ui-angular/angular/components/grid/grid",
       downloadLink: ""
     }],
     ['sales', {
       title: "Sales Dashboard",
       theme: "Indigo Light",
       content: "For trend analysis, KPIs and viewing sales summaries by region, product, etc.",
-      moreLink: "https://github.com/IgniteUI/grid-demos/tree/vnext/projects/sales-grid",
+      moreLink: "https://www.infragistics.com/products/ignite-ui-angular/angular/components/pivotGrid/pivot-grid",
       downloadLink: ""
     }],
     ['fleet', {
       title: "Fleet Management",
       theme: "Material Dark",
       content: "A master-detail grid for managing vehicle acquisition, operations, and maintenance.",
-      moreLink: "https://github.com/IgniteUI/grid-demos/tree/vnext/projects/fleet-management-grid",
+      moreLink: "https://www.infragistics.com/products/ignite-ui-angular/angular/components/grid/master-detail",
       downloadLink: ""
     }],
   ]);
 
   public onLinkClick(event: MouseEvent) {
     const targetHTML = event.currentTarget as HTMLAnchorElement;
-    if (!targetHTML.className.includes("--disabled")) {
-      window.open(targetHTML.href, '_blank')?.focus();
-    }
+    window.open(targetHTML.href, '_blank')?.focus();
 
     event.preventDefault();
     event.stopPropagation();
   }
 
   public onDownloadClick(event: MouseEvent, tabName: string) {
-    const targetHTML = event.currentTarget as HTMLAnchorElement;
-    if (!targetHTML.className.includes("--disabled")) {
-      console.log("Downloading app source!");
-    }
+    console.log("Downloading app source!");
 
     event.preventDefault();
     event.stopPropagation();
