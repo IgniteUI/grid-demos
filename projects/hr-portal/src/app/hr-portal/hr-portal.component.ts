e1387b615ed5f0356e44082622db25669263ac8b
Deyan Kamburov <dkamburov@infragistics.com>
2025-02-21 12:09:49 +0200

    chore(hr): remove merging leftovers

--- projects/hr-portal/src/app/hr-portal/hr-portal.component.ts
+++ projects/hr-portal/src/app/hr-portal/hr-portal.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Employee, EMPLOYEE_DATA } from '../data/localData';
 import {
   IgxTreeGridComponent,
