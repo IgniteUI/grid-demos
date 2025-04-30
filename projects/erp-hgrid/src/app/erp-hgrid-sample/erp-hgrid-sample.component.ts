e4fd98105b5646144b49d8f8cfa2078d8468fb3a
Mariela Tihova <MTihova@infragistics.com>
2025-04-30 13:27:37 +0300

    Added custom sorting on address columns

--- projects/erp-hgrid/src/app/erp-hgrid-sample/erp-hgrid-sample.component.ts
+++ projects/erp-hgrid/src/app/erp-hgrid-sample/erp-hgrid-sample.component.ts
@@ -111,6 +111,10 @@ export class ErpHGridSampleComponent implements AfterViewInit {
   public selectionMode: GridSelectionMode = 'multiple';
   public orderStatus = OrderStatus;
 
+  // Custom sorting strategy
+  public shortAddressSortStrategy = new AddressSortStrategy(this.formatAddress);
+  public fullAddressSortStrategy = new AddressSortStrategy(this.formatFullAddress);
+
   constructor(
     private iconService: IgxIconService,
     private erpDataService: ErpDataService
@@ -299,3 +303,26 @@ export class FullAddressFilteringOperand extends IgxStringFilteringOperand {
     this.operations = customOperations.concat(emptyOperators);
   }
 }
+
+export class AddressSortStrategy {
+  constructor(private formatter: (value: any) => string) {}
+
+  compareValues(a: any, b: any): number {
+    const addressA = this.formatter(a)?.toLowerCase() ?? '';
+    const addressB = this.formatter(b)?.toLowerCase() ?? '';
+    return addressA.localeCompare(addressB);
+  }
+
+  sort(data: any[], fieldName: string, sortingDirection: any): any[] {
+    if (!sortingDirection) return data;
+
+    return [...data].sort((rowA, rowB) => {
+      const valA = rowA[fieldName];
+      const valB = rowB[fieldName];
+      const compareResult = this.compareValues(valA, valB);
+      return sortingDirection === 1 ? compareResult : -compareResult;
+    });
+  }
+}
+
+
