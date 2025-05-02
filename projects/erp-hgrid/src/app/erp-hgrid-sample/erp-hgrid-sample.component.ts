9b6fe2e87d6e624e41dcd6af490600fb45d648e5
Mariela Tihova <MTihova@infragistics.com>
2025-05-02 16:10:28 +0300

    Removed sorting on Address Column in ERP Sample

--- projects/erp-hgrid/src/app/erp-hgrid-sample/erp-hgrid-sample.component.ts
+++ projects/erp-hgrid/src/app/erp-hgrid-sample/erp-hgrid-sample.component.ts
@@ -111,10 +111,6 @@ export class ErpHGridSampleComponent implements AfterViewInit {
   public selectionMode: GridSelectionMode = 'multiple';
   public orderStatus = OrderStatus;
 
-  // Custom sorting strategy
-  public shortAddressSortStrategy = new AddressSortStrategy(this.formatAddress);
-  public fullAddressSortStrategy = new AddressSortStrategy(this.formatFullAddress);
-
   constructor(
     private iconService: IgxIconService,
     private erpDataService: ErpDataService
@@ -303,26 +299,4 @@ export class FullAddressFilteringOperand extends IgxStringFilteringOperand {
     this.operations = customOperations.concat(emptyOperators);
   }
 }
-
-export class AddressSortStrategy {
-  constructor(private formatter: (value: any) => string) {}
-
-  compareValues(a: any, b: any): number {
-    const addressA = this.formatter(a)?.toLowerCase() ?? '';
-    const addressB = this.formatter(b)?.toLowerCase() ?? '';
-    return addressA.localeCompare(addressB);
-  }
-
-  sort(data: any[], fieldName: string, sortingDirection: any): any[] {
-    if (!sortingDirection) return data;
-
-    return [...data].sort((rowA, rowB) => {
-      const valA = rowA[fieldName];
-      const valB = rowB[fieldName];
-      const compareResult = this.compareValues(valA, valB);
-      return sortingDirection === 1 ? compareResult : -compareResult;
-    });
-  }
-}
-
 
