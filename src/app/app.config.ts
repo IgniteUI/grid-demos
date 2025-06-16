3fd7b4b20e195b3d8c1ae88f535f003bdcd10c97
Martin Dragnev <mddragnev@gmail.com>
2025-06-16 14:36:02 +0300

    update angular to v20

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { cacheInterceptor } from './cache.interceptor';
 
-
 export const appConfig: ApplicationConfig = {
-	providers: [
-    provideHttpClient(withInterceptors([cacheInterceptor]),),
+  providers: [
+    provideHttpClient(withInterceptors([cacheInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
     provideRouter(routes),
-    provideAnimations()]
+    provideAnimations(),
+  ],
 };
