187150aeaf6d9752eae3acb774e59cbaf336cafb
Martin Dragnev <mddragnev@gmail.com>
2025-06-18 14:45:29 +0300

    Main app: update main app ssr configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { cacheInterceptor } from './cache.interceptor';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([cacheInterceptor])),
+    provideHttpClient(withInterceptors([cacheInterceptor]), withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
     provideRouter(routes),
-    provideAnimations(),
+    provideAnimations(), provideClientHydration(withEventReplay()),
   ],
 };
