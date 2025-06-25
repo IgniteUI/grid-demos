4bd624c00a687631ed5cd2db3eb7994d38c24654
Martin Dragnev <37667452+mddragnev@users.noreply.github.com>
2025-06-25 19:12:13 +0300

    Merge pull request #114 from IgniteUI/mdragnev/try-ssr
    Update the main applicatin and standalone samples to use SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { cacheInterceptor } from './cache.interceptor';
-
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-    provideHttpClient(withInterceptors([cacheInterceptor]),),
+  providers: [
+    provideHttpClient(withInterceptors([cacheInterceptor]), withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
     provideRouter(routes),
-    provideAnimations()]
+    provideAnimations(), provideClientHydration(withEventReplay()),
+  ],
 };
