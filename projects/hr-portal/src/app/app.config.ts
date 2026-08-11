c486153137d030aec33da780a0b2c453ff9b5baa
Martin Dragnev <mddragnev@gmail.com>
2026-08-11 15:31:31 +0300

    chore(hr-portal): update to latest angular and igniteui-angular

--- projects/hr-portal/src/app/app.config.ts
+++ projects/hr-portal/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withNoIncrementalHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withFetch()),
-    provideClientHydration(),
+    provideClientHydration(withNoIncrementalHydration()),
     provideAnimations(),
   ],
 };
