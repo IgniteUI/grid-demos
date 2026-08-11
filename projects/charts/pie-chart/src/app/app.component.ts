39671dfe5ea95dcb01fc87fabd3ff12e42be56de
Martin Dragnev <mddragnev@gmail.com>
2026-08-11 16:53:27 +0300

    chore(pie-chart): update to latest angular

--- projects/charts/pie-chart/src/app/app.component.ts
+++ projects/charts/pie-chart/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
 import { PieChartDemoComponent } from './pie-chart-sample/pie-chart-sample.component';
 
@@ -8,6 +8,7 @@ import { PieChartDemoComponent } from './pie-chart-sample/pie-chart-sample.compo
   imports: [PieChartDemoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
